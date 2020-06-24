13a545eb037cb5e7501d31a61eb9e164d440a57a
irlen <wangyangruoshi@163.com>
2020-06-24 22:59:27 +0800

    'haha'

--- src/router/routerConfig.js
+++ src/router/routerConfig.js
@@ -9,35 +9,35 @@ import { GiCardExchange } from 'react-icons/gi'
 export const routerConfig = [
   {
     path:"/app/change",
-    name:"安全变更",
+    name:"类目",
     icon: <ControlFilled />,
     key:"/app/change",
     children:[
-      {path:"/app/change/changerequest",key:"changerequest",name:"变更请求",icon: <span style={{verticalAlign:"middle",marginRight:"10px"}}><GiCardExchange /></span>},
-      {path:"/app/change/changetask",key:"changetask",name:"变更任务",icon: <span style={{verticalAlign:"middle",marginRight:"10px"}}><FaTasks /></span>},
+      {path:"/app/change/changerequest",key:"changerequest",name:"类目列表",icon: <span style={{verticalAlign:"middle",marginRight:"10px"}}><GiCardExchange /></span>},
+      //{path:"/app/change/changetask",key:"changetask",name:"变更任务",icon: <span style={{verticalAlign:"middle",marginRight:"10px"}}><FaTasks /></span>},
       //{path:"/app/change/changesecurity",key:"changesecurity",name:"安全变更",icon: <span style={{verticalAlign:"middle",marginRight:"10px"}}><MdSecurity /></span>},
     ]
   },
   {
     path:"/app/look",
-    name:"配置查看",
+    name:"商品",
     icon: <EyeFilled />,
     key:"/app/look",
     children:[
-      {path:"/app/look/lookset",key:"lookset",name:"ACL及策略",icon: <span style={{verticalAlign:"middle",marginRight:"10px"}}><FaRegListAlt /></span>},
+      {path:"/app/look/lookset",key:"lookset",name:"商品列表",icon: <span style={{verticalAlign:"middle",marginRight:"10px"}}><FaRegListAlt /></span>},
     ]
   },
   {
     path:"/app/set",
-    name:"系统设置",
+    name:"订单",
     icon: <SettingFilled />,
     key:"/app/set",
     children:[
-      {path:"/app/set/setmonitor",key:"setmonitor",name:"监控设备",icon: <span style={{verticalAlign:"middle",marginRight:"10px"}}><FaServer /></span>},
-      {path:"/app/set/setgroup",key:"setgroup",name:"分组",icon: <span style={{verticalAlign:"middle",marginRight:"10px"}}><FaLayerGroup /></span>},
-      {path:"/app/set/setuser",key:"setuser",name:"用户",icon: <span style={{verticalAlign:"middle",marginRight:"10px"}}><FaUserCog /></span>},
-      {path:"/app/set/setauth",key:"setauth",name:"外部认证",icon: <span style={{verticalAlign:"middle",marginRight:"10px"}}><FaBarcode /></span>},
-      {path:"/app/set/setplatform",key:"setplatform",name:"平台配置",icon: <span style={{verticalAlign:"middle",marginRight:"10px"}}><IoIosSwitch /></span>},
+      {path:"/app/set/setmonitor",key:"setmonitor",name:"订单列表",icon: <span style={{verticalAlign:"middle",marginRight:"10px"}}><FaServer /></span>},
+      // {path:"/app/set/setgroup",key:"setgroup",name:"分组",icon: <span style={{verticalAlign:"middle",marginRight:"10px"}}><FaLayerGroup /></span>},
+      // {path:"/app/set/setuser",key:"setuser",name:"用户",icon: <span style={{verticalAlign:"middle",marginRight:"10px"}}><FaUserCog /></span>},
+      // {path:"/app/set/setauth",key:"setauth",name:"外部认证",icon: <span style={{verticalAlign:"middle",marginRight:"10px"}}><FaBarcode /></span>},
+      // {path:"/app/set/setplatform",key:"setplatform",name:"平台配置",icon: <span style={{verticalAlign:"middle",marginRight:"10px"}}><IoIosSwitch /></span>},
 
     ]
   }
